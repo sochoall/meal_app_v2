40983162058c55554e43df7973ed4c73eda31b6f
sochoall <sochoa@libelulasoft.com>
2023-08-26 19:25:18 -0500

    REFACTOR: Added providerScope to detect changes on the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:meal_app_v2/screens/tabs.dart';
 
 final theme = ThemeData(
@@ -13,7 +14,9 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(const ProviderScope(
+    child: App(),
+  ));
 }
 
 class App extends StatelessWidget {
